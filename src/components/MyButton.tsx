c6e037d9cac057f2d87b347f94f58ebb9c4b8874
yongun2 <djkye0001@naver.com>
2023-12-04 01:53:47 +0900

    project init

---
+++ src/components/MyButton.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const MyButton = (props: Props) => {
+  return (
+    <View>
+      <Text>MyButton</Text>
+    </View>
+  );
+};
+
+export default MyButton;
+
+const styles = StyleSheet.create({});
