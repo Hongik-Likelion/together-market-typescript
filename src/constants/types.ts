1679ea2839714a96de99791b8fc82c6d61ece3b4
yongun2 <djkye0001@naver.com>
2023-12-05 19:30:39 +0900

    [styles] 메인 색상 추가

---
+++ src/constants/types.ts
@@ -0,0 +1,13 @@
+export type GlobalStylesType = {
+  colors: {
+    main01: "#35AE83";
+    main02: "#C9FFEB";
+    main03: "#E4FFF5";
+    red: "#FF7354";
+    lightgray: "#EEEEEE";
+    gray01: "#B5B5B5";
+    gray02: "#EAEAEA";
+    darkgray01: "#393939";
+    darkgray02: "#666";
+  };
+};
