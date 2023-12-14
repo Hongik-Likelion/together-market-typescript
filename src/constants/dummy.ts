339b38cdc9adf2531559297b87003578a1943b39
yongun2 <djkye0001@naver.com>
2023-12-14 12:24:49 +0900

    [dummy] 더미 데이터

---
+++ src/constants/dummy.ts
@@ -0,0 +1,18 @@
+export const markets = [
+  {
+    id: 1,
+    name: "방신시장",
+  },
+  {
+    id: 2,
+    name: "방화시장",
+  },
+  {
+    id: 3,
+    name: "망원시장",
+  },
+  {
+    id: 4,
+    name: "강남시장",
+  },
+];
